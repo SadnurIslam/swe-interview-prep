edc88fb0df0da961c163863cdd27160d89858f37
sadnur Islam <sadnurislam2000@gmail.com>
2026-02-18 19:22:40 +0600

    alternating bits

---
+++ 01-dsa/bit-manipulation/Alternating Bits.cpp
@@ -0,0 +1,18 @@
+/*
+Daily problem practice
+Link: https://leetcode.com/problems/binary-number-with-alternating-bits/description
+*/
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev = -1;
+        while(n>0){
+            int rem = n%2;
+            if(rem==prev)return false;
+            n/=2;
+            prev = rem;
+        }
+        return true;
+    }
+};
